fd438feb40c0bb380848489ee2c6627bded16731
GarrisonJohnston123 <Garrison.L.Johnston@vanderbilt.edu>
2026-07-07 11:59:09 -0500

    created starter files.

--- eigen_and_launch/include/eigen_and_launch/sine_wave_subscriber.h
+++ eigen_and_launch/include/eigen_and_launch/sine_wave_subscriber.h
@@ -1,32 +1,8 @@
 #ifndef SINE_WAVE_SUBSCRIBER
 #define SINE_WAVE_SUBSCRIBER
 
-#include <rclcpp/rclcpp.hpp>
-#include <Eigen/Dense>
-#include <eigen_and_launch/msg/sine_wave_stamped.hpp>
+// ADD INCLUDES HERE
 
-/**
- * Subscribes to /sine_wave topic and prints to terminal
- * Applied Robotics
- * Author: Garrison Johnston
- */
-class SineWaveSubscriber : public rclcpp::Node
-{
-public:
-    SineWaveSubscriber();
-    ~SineWaveSubscriber() = default;
-
-private:
-    // runs automatically when sine wave topic updates
-    void sineWaveCallback(const eigen_and_launch::msg::SineWaveStamped & msg);
-
-    // Publishers/subscribers
-    rclcpp::Subscription<eigen_and_launch::msg::SineWaveStamped>::SharedPtr subscriber_;
-
-    // initialize members
-    Eigen::Vector2d z_k_;
-    Eigen::Vector2d vec_;
-    Eigen::Matrix2d mat_;
-};
+// DECLARE CLASS HERE
 
 #endif // SINE_WAVE_SUBSCRIBER
